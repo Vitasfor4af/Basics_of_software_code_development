e919eb81829522ef84a2c5255bb1d63c038858c4
Vitaliy <vitaliy.g989@gmail.com>
2019-06-14 00:19:32 +0300

    version 1.4

--- Task10/src/Task.java
+++ Task10/src/Task.java
@@ -8,13 +8,19 @@ public class Task {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.print("Enter the dimension of the rectangular hole A = "); float a = scn.nextFloat();
-        System.out.print("Enter the dimension of the rectangular hole B = "); float b = scn.nextFloat();
-        System.out.print("Enter the dimension of the brick x(length) = "); float x = scn.nextFloat();
-        System.out.print("Enter the dimension of the brick y(width) = "); float y = scn.nextFloat();
-        System.out.print("Enter the dimension of the brick z(height) = "); float z = scn.nextFloat();
-        if ((y < b && z < a) || (z < b && y < a) || (x < b && z < a))
-             System.out.println(true);
+        System.out.print("Enter the dimension of the rectangular hole A = ");
+        float a = scn.nextFloat();
+        System.out.print("Enter the dimension of the rectangular hole B = ");
+        float b = scn.nextFloat();
+        System.out.print("Enter the dimension of the brick x(length) = ");
+        float x = scn.nextFloat();
+        System.out.print("Enter the dimension of the brick y(width) = ");
+        float y = scn.nextFloat();
+        System.out.print("Enter the dimension of the brick z(height) = ");
+        float z = scn.nextFloat();
+        if ((y < b && z < a) || (x < b && z < a) || (z < b && y < a) || (x < b && y < a) || (z < b && x < a) ||
+                (y < b && x < a))
+            System.out.println(true);
         else System.out.println(false);
     }
 }
