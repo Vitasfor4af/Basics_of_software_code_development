1134684e7b10aba2015c8194861450d532e15045
Vitaliy <vitaliy.g989@gmail.com>
2019-06-26 16:39:48 +0300

    version 1.6

---
+++ src/by/epam/linePrograms/Task1/Task.java
@@ -0,0 +1,23 @@
+package by.epam.linePrograms.one;
+
+import java.util.Scanner;
+
+/* Найдите значение функции: z = ( (a – 3 ) * b / 2) + c. */
+
+public class Task {
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Input the values of a, b and c, correspondingly");
+        float a = scn.nextFloat();
+        float b = scn.nextFloat();
+        float c = scn.nextFloat();
+        System.out.println("result = " + compute(a, b, c));
+    }
+
+    public static float compute(float a, float b, float c){
+        float result = ((a - 3) * b / 2) + c;
+    return result;
+    }
+}
